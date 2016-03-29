d33f6bb37616ef51036f32364df594349f8b3bf8
Kiea Seok Kang <grtlinux@gmail.com>
2016-03-29 10:44:22 +0900

    .

---
+++ WEB_TEST01/src/tain/kr/common/Version.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright 2014, 2015, 2016 TAIN, Inc. all rights reserved.
+ *
+ * Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * -----------------------------------------------------------------
+ * Copyright 2014, 2015, 2016 TAIN, Inc.
+ *
+ */
+package tain.kr.common;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Code Templates > Comments > Types
+ *
+ * <PRE>
+ *   -. FileName   : Version.java
+ *   -. Package    : tain.kr.common
+ *   -. Comment    :
+ *   -. Author     : taincokr
+ *   -. First Date : 2016. 3. 29. {time}
+ * </PRE>
+ *
+ * @author taincokr
+ *
+ */
+public class Version {
+
+	private static boolean flag = true;
+
+	private static final Logger log = Logger.getLogger(Version.class);
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+}
