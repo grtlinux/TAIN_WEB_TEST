6ce6e79843b0925bdb6ad18d3790be97650bddc7
Kiea Seok Kang <grtlinux@gmail.com>
2016-04-18 19:10:40 +0900

    .

---
+++ WEB_TEST01/src/tain/kr/cosmarter/client/v01/CSN00001.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright 2014, 2015, 2016 TAIN, Inc. all rights reserved.
+ *
+ * Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * -----------------------------------------------------------------
+ * Copyright 2014, 2015, 2016 TAIN, Inc.
+ *
+ */
+package tain.kr.cosmarter.client.v01;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Code Templates > Comments > Types
+ *
+ * <PRE>
+ *   -. FileName   : CSN00001.java
+ *   -. Package    : tain.kr.cosmarter.client.v01
+ *   -. Comment    :
+ *   -. Author     : taincokr
+ *   -. First Date : 2016. 4. 18. {time}
+ * </PRE>
+ *
+ * @author taincokr
+ *
+ */
+public class CSN00001 {
+
+	private static boolean flag = true;
+
+	private static final Logger log = Logger.getLogger(CSN00001.class);
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+}
